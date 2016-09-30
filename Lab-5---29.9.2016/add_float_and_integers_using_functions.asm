3bdca0ad571cfbcf68810f96d73f8f2186a3a8a5
adhitya97 <adhitya.rajgopal@gmail.com>
2016-10-01 01:10:29 +0400

    Add two float and integer values using functions

--- Lab-5---29.9.2016/add_float_and_integers_using_functions.asm
+++ Lab-5---29.9.2016/add_float_and_integers_using_functions.asm
@@ -5,18 +5,17 @@
 	num3: .float 4.444
 	num4: .float 32.29
 
-	res1: .word 0
-	res2: .float 0.0
-
 	msg1: .asciiz "Sum of 34 and 21 = "
-	msg2: .asciiz "Sum of 4.444 and 32.29 = "
+	msg2: .asciiz "\nSum of 4.444 and 32.29 = "
 
 .text
 .globl main
 .ent main
 main:
+
 	lw $a0, num1
 	lw $a1, num2
+	# Calling function to add two integers
 	jal int_add
 	move $a2,$v0
 	li $v0, 4
@@ -25,6 +24,19 @@ main:
 	move $a0,$a2
 	li $v0,1
 	syscall	
+
+	l.s $f12, num3
+	l.s $f14, num4
+	# Calling function to add two float values
+	jal float_add
+	li $v0, 4
+	la $a0, msg2
+	syscall	
+	mov.s $f12,$f0
+	li $v0,2
+	syscall	
+
+	# Exit Program
 	li $v0, 10
 	syscall
 .end main
@@ -39,5 +51,6 @@ int_add:
 .globl float_add
 .ent float_add
 float_add:
-
+	add.s $f0,$f12,$f14
+	jr $ra
 .end float_add
