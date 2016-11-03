00318ca6865fefd07e23389dcf1d1e5f4e6db9e6
Adhitya Rajagopalan <adhitya.rajgopal@gmail.com>
2016-11-03 01:11:28 -0700

    Test the circuit

---
+++ Lab-8---3.11.2016/Question-3/circuit_tester.v
@@ -0,0 +1,27 @@
+module circuit_tester;
+	wire o;
+	reg a,b,s;
+	mux c1(o,a,b,s);
+	initial
+	begin
+		a=0;b=0;s=0;
+		#20
+		a=0;b=0;s=1;
+		#20	
+		a=0;b=1;s=0;
+		#20
+		a=0;b=1;s=1;
+		#20
+		a=1;b=0;s=0;
+		#20
+		a=1;b=0;s=1;
+		#20
+		a=1;b=1;s=0;
+		#20
+		a=1;b=1;s=1;
+		#20
+		a=0;b=0;s=0;		
+	end
+	initial
+		$monitor("A=%b B=%b S=%b out=%b time=%0d", a, b, s, o, $time);
+endmodule
